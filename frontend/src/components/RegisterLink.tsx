46143639f5e6160fd29cc0eebd0bf55b2fb4a6c5
valdeci <moura97.valdeci@gmail.com>
2023-09-14 15:54:47 -0300

    refactor(components): create register link button own file

---
+++ frontend/src/components/RegisterLink.tsx
@@ -0,0 +1,10 @@
+export default function RegisterLink() {
+  return (
+    <a
+      href="/register"
+      className="flex justify-center items-center w-225 bg-btngreen h-50 rounded-md text-white text-fgl"
+    >
+      Cadastre-se
+    </a>
+  );
+}
