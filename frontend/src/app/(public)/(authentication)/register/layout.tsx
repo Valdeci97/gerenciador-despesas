ed393bfaff04cf031361dd908ff7f270e2e4537c
valdeci <moura97.valdeci@gmail.com>
2023-10-02 21:23:21 -0300

    feat(pages): create register layout and page draft

---
+++ frontend/src/app/(public)/(authentication)/register/layout.tsx
@@ -0,0 +1,29 @@
+import { Metadata } from 'next';
+import { Spline_Sans } from 'next/font/google';
+
+import Providers from '@/components/Providers';
+import { ChildrenProps } from '@/@types/children-props';
+
+import '../../globals.css';
+
+const splineSans = Spline_Sans({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'Organiza Real - Registro',
+  description: 'Crie sua conta e comece já a organizar sua vida financeira',
+};
+
+export default function RegisterLayout({ children }: ChildrenProps) {
+  return (
+    <html
+      lang="pt-BR"
+      className="light"
+      style={{ colorScheme: 'light' }}
+      suppressHydrationWarning
+    >
+      <body className={`${splineSans.className} bg-light dark:bg-dark`}>
+        <Providers>{children}</Providers>
+      </body>
+    </html>
+  );
+}
---
+++ frontend/src/app/(public)/(authentication)/register/page.tsx
@@ -0,0 +1,18 @@
+import RegisterForm from '@/components/RegisterForm';
+
+export default function Register() {
+  return (
+    <main className="flex flex-col justify-center items-center h-screen gap-y-30px">
+      <RegisterForm />
+      <p className="text-20px text-light dark:text-white smdvc-400px:text-14px mddvc-540px:text-16px dvc-brkpt-768px:text-18px">
+        Já tenho uma conta.&nbsp;
+        <a
+          href="/login"
+          className="text-20px text-green-contrast smdvc-400px:text-14px mddvc-540px:text-16px dvc-brkpt-768px:text-18px"
+        >
+          Quero fazer login!
+        </a>
+      </p>
+    </main>
+  );
+}
